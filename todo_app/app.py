f3b23b0d199021e109bfec3343979b4c4d2be5bb
Giles <Giles.Waterworth@capita.co.uk>
2021-02-19 14:40:49 +0000

    updated_index.thml

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from todo_app.flask_config import Config
+from todo_app.flask_config import Config, render_template
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +8,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.hmtl')
 
 
 if __name__ == '__main__':
