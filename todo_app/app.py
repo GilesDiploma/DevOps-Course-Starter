212084d14142ed3b3afac4d54eb290d7e3a10312
Giles <Giles.Waterworth@capita.co.uk>
2021-02-25 13:55:37 +0000

    form_added

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +9,14 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = get_items()
+    return render_template('index.html',items=items)
+
+@app.route('/new', methods=['POST'])
+def new():
+    add_item('new item')
+    return redirect(url_for('index'))
+    
 
 
 if __name__ == '__main__':
