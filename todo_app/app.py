23e0e1c4a098a598371261a5b9c3c13235d2eb4a
Giles <Giles.Waterworth@capita.co.uk>
2021-03-07 17:39:11 +0000

    newupdates

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
@@ -7,15 +7,15 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def index():
     items = get_items()
     return render_template('index.html',items=items)
 
 @app.route('/new', methods=['GET','POST'])
-def new():
-    add_item('item')
-    item = request.form.get('submit')
+def new():    
+    title = request.form.get('name')
+    add_item(title)
     return redirect(url_for('index'))
     
 if __name__ == '__main__':
